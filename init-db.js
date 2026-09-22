10d2481486fedc9f01eb0dc7fbd7d1ad5900a0e0
salma-code-it <salmabelaicha665@gmail.com>
2026-09-22 11:53:43 +0200

    Update init-db.js

--- init-db.js
+++ init-db.js
@@ -9,7 +9,7 @@ async function initializeDatabase() {
         connection = await mysql.createConnection({
             host: 'localhost',
             user: 'root',
-            password: '#salmasalma!@',
+            password: '',
             ssl: false
         });
 
